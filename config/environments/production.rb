d65e376ee10f59e2695bc35c1d8de62804da3eae
Weiee Ying <yingweiee@gmail.com>
2015-09-11 11:31:47 +0800

    Added AWS S3 for Paperclip hosting images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
   # For Heroku Devise
   # Note to set this to actual host
   config.action_mailer.default_url_options = { host: 'weiee-pinteresting.herokuapp.com' }
+
+  # Sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
