b672fa5a8e52f7b6604cbf6dce90f2f8994d1b33
Weiee Ying <yingweiee@gmail.com>
2015-09-14 11:05:33 +0800

    Changed order of application.js & replaced code in Pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 50)
   end
 
   def show
@@ -19,26 +19,24 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-      if @pin.save
-        redirect_to @pin, notice: 'Pin was successfully created.'
-      else
-        render action: 'new'
-      end
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
-      if @pin.update(pin_params)
-        redirect_to @pin, notice: 'Pin was successfully updated.'
-      else
-        render action: 'edit' 
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
-  # DELETE /pins/1
-  # DELETE /pins/1.json
   def destroy
     @pin.destroy
-    redirect_to :back
+    redirect_to pins_url
   end
 
   private
@@ -47,10 +45,10 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-  def correct_user
-    @pin = current_user.pins.find_by(params[:id])
-    redirect_to pins_path, notice: "Not authorised to edit this pin." if @pin.nil?
-  end
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
